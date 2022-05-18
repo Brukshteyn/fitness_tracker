da883739b1da84f7d669b45a5df77b0f5d4ddc0d
Brukshteyn <r.brukshteyn@gmail.com>
2022-05-18 20:08:23 +0300

    initial structure

---
+++ main.py
@@ -0,0 +1,41 @@
+class InfoMessage:
+
+
+    def __init__(self):
+        pass
+
+
+class Tranning:
+
+
+    def __init__(self):
+        pass
+
+    def get_distance(self):
+        pass
+
+    def get_spent_calories(self):
+        pass
+
+
+class Swimming:
+
+
+    def get_spent_calories(self):
+        pass
+
+    def get_mean_speed(self):
+        pass
+
+
+class SportsWalking:
+    pass
+
+
+class Running:
+    pass
+
+
+if __name__ == '__main__':
+    pass
+

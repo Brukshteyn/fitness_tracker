7e78f111e3fe426d49608a67afe38ba6fe192459
Brukshteyn <r.brukshteyn@gmail.com>
2022-05-20 14:13:55 +0300

    complete ver.1

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
+from typing import Tuple, List
+
+
 class InfoMessage:
 
+    MESSAGE = '''
+    Тип тренировки: {tranning_type}; 
+    Длительность: {duration} ч;
+    Дистанция: {distance} км;
+    Ср. скорость: {speed} км/ч;
+    Потрачено ккал: {calories}'''
 
     def __init__(self,
-                 training_type : str,
+                 tranning_type : str,
                  duration : float,
                  distance : float,
                  speed : float,
                  calories : float) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+        self.tranning_type = tranning_type
+        self.duration = round(duration, 3)
+        self.distance = round(distance, 3)
+        self.speed = round(speed, 3)
+        self.calories = round(calories, 3)
+
+    def get_info_message(self) -> str:
+        return self.MESSAGE.format(tranning_type = self.tranning_type,
+                                   duration = self.duration,
+                                   distance = self.distance,
+                                   speed = self.speed,
+                                   calories = self.calories)
 
 
 class Tranning:
 
     M_IN_KM = 1000
     LEN_STEP = 0.65
+    NAME_TRANNING = 'noname'
 
-    def __init__(self, action : int, duration : float, weight : float) -> None:
+    def __init__(self, action : int, duration : int, weight : int) -> None:
         self.action = action
         self.duration = duration
         self.weight = weight
@@ -33,15 +50,21 @@ class Tranning:
     def get_spent_calories(self) -> float:
         pass
 
-    def show_training_info(self) -> None:
-        pass
+    def show_training_info(self) -> InfoMessage:
+        return InfoMessage(tranning_type = self.NAME_TRANNING,
+                           duration = self.duration,
+                           distance = self.get_distance(),
+                           speed = self.get_mean_speed(),
+                           calories = self.get_spent_calories())
 
 
 class Swimming(Tranning):
 
     LEN_STEP = 1.38
+    NAME_TRANNING = 'Плавание'
 
-    def __init__(self, length_pool : int, count_pool : int):
+    def __init__(self, action : int, duration : int, weight : int, length_pool : int, count_pool : int) -> None:
+        super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
 
@@ -55,8 +78,10 @@ class Swimming(Tranning):
 
 class SportsWalking(Tranning):
 
+    NAME_TRANNING = 'Спортивная ходьба'
 
-    def __init__(self, height : int) -> None:
+    def __init__(self, action : int, duration : int, weight : int, height : int) -> None:
+        super().__init__(action, duration, weight)
         self.height = height
 
     def get_spent_calories(self) -> float:
@@ -67,6 +92,7 @@ class SportsWalking(Tranning):
 
 class Running(Tranning):
 
+    NAME_TRANNING = 'Бег'
 
     def get_spent_calories(self) -> float:
         coeff_calorie_1 = 18
@@ -74,6 +100,37 @@ class Running(Tranning):
         return ((coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2 ) *self.weight) / self.M_IN_KM * self.duration
 
 
+def main(tranning: Tranning) -> None:
+    info = tranning.show_training_info()
+    print(info.get_info_message())
+
+def read_package(tranning_type : str,  package : List[int]) -> Tranning:
+    if tranning_type == 'RUN':
+        return Running(action = package[0],
+                       duration = package[1],
+                       weight = package[2])
+    if tranning_type == 'SWM':
+        return Swimming(action = package[0],
+                        duration = package[1],
+                        weight = package[2],
+                        length_pool = package[3],
+                        count_pool = package[4]
+                        )
+    if tranning_type == 'WLK':
+        return SportsWalking(action = package[0],
+                             duration = package[1],
+                             weight = package[2],
+                             height = package[3])
+
 if __name__ == '__main__':
-    pass
+    packages = [
+        ('SWM', [720, 1, 80, 25, 40]),
+        ('RUN', [15000, 1, 75]),
+        ('WLK', [9000, 1, 75, 180]),
+    ]
+
+    for workout_type, data in packages:
+        training = read_package(workout_type, data)
+        main(training)
+
 
